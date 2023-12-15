870bdd61412631470f07a6c6b75f38c1ffb7e588
armmike <129191816+armmike@users.noreply.github.com>
2023-12-15 16:31:09 +0100

    Update 0#_ElFamosoFIZZBUZZ_Py.py

--- 0#_ElFamosoFIZZBUZZ_Py.py
+++ 0#_ElFamosoFIZZBUZZ_Py.py
@@ -1,6 +1,6 @@
+#@armmike
+###########################################
 '''
-MK4
-
 Reto #0: EL FAMOSO “FIZZ BUZZ” 
 Dificultad: Fácil | Publicación: 26/12/22 | Corrección: 02/01/23 
 Enunciado 
@@ -10,17 +10,17 @@ Enunciado
  - Múltiplos de 3 por la palabra "fizz".
  - Múltiplos de 5 por la palabra "buzz".
  - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-
-
 '''
+###########################################
 #define main function to print out something
 print('Escribe un programa que muestre por consola (con un print) los\
  números de 1 a 100 (ambos incluidos y con un salto de línea entre\
  cada impresión)')
-
+###########################################
 def fizzbuzz():
     i = 1
     max = 101
+    ###########################################
     while (i < max):
         #x3 fizz 
         #x5 buzz
@@ -33,10 +33,12 @@ def fizzbuzz():
             print('buzz','\n')
         else:
             print(i,'\n')
-
+        ###########################################
         i = i + 1
-
+     ###########################################
+###########################################
 def fizzbuzz02():
+    ###########################################
     for n in range(1,101):
         if (n % 3 == 0) and (n % 5 == 0): 
             print('fizzbuzz', '\n')
@@ -46,8 +48,9 @@ def fizzbuzz02():
             print('buzz', '\n')
         else:
             print(n, '\n')
-        
+    ###########################################    
 #call function main
 fizzbuzz()
 fizzbuzz02()
-
+###########################################
+#@armmike
